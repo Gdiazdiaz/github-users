3a20e7c59babafb5a4f9888ad7b6f0ac9c12dec3
Glenda Diaz <glendadiazah@outlook.com>
2024-10-20 14:59:46 -0600

    feat: add missing actions and modify effects to fit requiremnets

--- src/app/store/search/search.reducer.ts
+++ src/app/store/search/search.reducer.ts
@@ -17,12 +17,22 @@ export const searchReducer = createReducer(
   on(SearchActions.loadSearch, (state) => ({ ...state, loading: true })),
   on(SearchActions.loadSearchSuccess, (state, { search }) => ({
     ...state,
-    search,
+    result: search,
     loading: false,
   })),
   on(SearchActions.loadSearchFailure, (state, { error }) => ({
     ...state,
     error,
+    loading: false,
+  })),
+  on(SearchActions.clearSearch, (state) => ({
+    ...state,
+    result: {
+      ...state.result,
+      items: [],  // Clear items
+      total_count: 0,
+      incomplete_results: false
+    },
     loading: false,
   }))
 );
--- src/app/store/user/user.effects.ts
+++ src/app/store/user/user.effects.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
+import { catchError, map, mergeMap, switchMap } from 'rxjs/operators';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import * as UserActions from './user.actions';
 import { environment } from 'src/environments/environment';
 import { User, UserByUsername } from 'src/app/interfaces/user';
@@ -15,14 +15,34 @@ export class UserEffects {
   loadUsers$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UserActions.loadUsers),
-      mergeMap((action) =>
-        this.http.get<User[]>(`${this.baseUrl}?per_page=${action.per_page}`).pipe(
-          map((users: User[]) => UserActions.loadUsersSuccess({ users })), 
-          catchError((error) => of(UserActions.loadUsersFailure({ error })))
+      switchMap(({ since, per_page }) =>
+        this.http.get<User[]>(this.baseUrl, {
+          params: { since: since.toString(), per_page: per_page.toString() },
+          observe: 'response',
+        }).pipe(
+          map((response: HttpResponse<User[]>) => {
+            const users = response.body || [];
+            const linkHeader = response.headers.get('Link');
+            const nextSince = this.getNextSince(linkHeader);
+            console.log(nextSince, 'since')
+            return UserActions.loadUsersSuccess({ users, since: nextSince.toString() });
+          }),
+          catchError(error => of(UserActions.loadUsersFailure({ error: error.message })))
         )
       )
     )
   );
+
+  private getNextSince(linkHeader: string | null): number {
+    if (linkHeader) {
+      const nextLink = linkHeader.split(',').find(link => link.includes('rel="next"'));
+      if (nextLink) {
+        const sinceMatch = nextLink.match(/since=(\d+)/);
+        return sinceMatch ? parseInt(sinceMatch[1], 10) : 0;
+      }
+    }
+    return 0;
+  }
 
   loadUser$ = createEffect(() =>
     this.actions$.pipe(
@@ -36,3 +56,4 @@ export class UserEffects {
     )
   );
 }
+
